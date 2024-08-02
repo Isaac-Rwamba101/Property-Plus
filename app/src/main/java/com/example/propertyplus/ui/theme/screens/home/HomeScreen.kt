31cb9c4471247c574124fa5c52b22c3260e2cc23
vivybutterflysparkles <opindivivian@gmail.com>
2024-08-02 11:16:31 +0300

    commit

--- app/src/main/java/com/example/propertyplus/ui/theme/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/propertyplus/ui/theme/screens/home/HomeScreen.kt
@@ -1,20 +1,45 @@
 package com.example.propertyplus.ui.theme.screens.home
 
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.propertyplus.ui.theme.BlueIvy
 
 
 @Composable
 fun HomeScreen(navController: NavController){
 
-    Column (modifier = Modifier.fillMaxSize()){
+    Column (
+        modifier = Modifier.fillMaxSize().background(BlueIvy),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+        Text(
+            text = "Property Plus",
+            fontSize = 60.sp,
+            fontFamily = FontFamily.Cursive,
+            color = Color.Magenta,
+        )
+
+        Text(
+            text = "Your better choice",
+            fontSize = 20.sp,
+
+        )
 
 
     }
